de8e0ba860c1117873530caa50a40f83565b36e7
Ryan <Ryan@RYAN-LAPTOP>
2015-01-22 01:35:13 +0000

    Testing linking directly into TFM command handling.

--- src/com/superiornetworks/icarus/commands/Command_icarusmod.java
+++ src/com/superiornetworks/icarus/commands/Command_icarusmod.java
@@ -1,25 +1,27 @@
 package com.superiornetworks.icarus.commands;
 
-import net.pravian.bukkitlib.command.BukkitCommand;
-import net.pravian.bukkitlib.command.CommandPermissions;
-import net.pravian.bukkitlib.command.SourceType;
+import me.StevenLawson.TotalFreedomMod.Commands.AdminLevel;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandParameters;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions;
+import me.StevenLawson.TotalFreedomMod.Commands.SourceType;
+import me.StevenLawson.TotalFreedomMod.Commands.TFM_Command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-@CommandPermissions(source = SourceType.ANY, permission = "")
-public class Command_icarusmod extends BukkitCommand
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Goto the ender / \"The End\".", usage = "/<command>")
+public class Command_icarusmod extends TFM_Command
 {
 
     @Override
-    public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-
         // This is currently just crap to test command loading. I will make it do something more impressive at some point in the future, but for now it will have to do...
-        commandSender.sendMessage("Welcome to the Icarus Project");
-        commandSender.sendMessage("We are hoping to start something amazing!");
-        commandSender.sendMessage("This is more of a test command for now, so just ignore it ;) !");
+        sender.sendMessage("Welcome to the Icarus Project");
+        sender.sendMessage("We are hoping to start something amazing!");
+        sender.sendMessage("This is more of a test command for now, so just ignore it ;) !");
         return true;
-
     }
 
 }
